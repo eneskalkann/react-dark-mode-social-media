ab415d26be4fbff9283185035682bfc693815762
eneskalkann <eneskalkan36e@gmail.com>
2022-10-10 22:22:48 +0300

    adding-react-switch
    adding react switch and change background colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React, { useState } from 'react';
+import Dashboard from './components/Dashboard';
+import { createContext } from 'react';
+import ReactSwitch from 'react-switch';
+
+export const DarkModeContext = createContext(null);
 
 function App() {
+
+  const [theme , setTheme] = useState("light");
+  const Toggle = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"))
+  }
+
   return (
-    <div className="App">
+    <DarkModeContext.Provider value={{theme,Toggle}}>
+    <div className="App" class={theme}>
+      <Dashboard/>
+      <div className="toggle">
+      <ReactSwitch onChange={Toggle} onColor={'378FE6'} offColor={'378FE6'} checked={theme === "dark"}/>
+      </div>
     </div>
+    </DarkModeContext.Provider>
   );
 }
 
